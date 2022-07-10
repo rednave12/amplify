570d709a7355912ff738ab35b957651062105fa1
Evander <evander.rt@btinternet.com>
2022-07-10 13:45:13 +0100

    changed name to Evander

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ function App() {
 
     return (
         <div className="App">
-        <h1>My Notes App</h1>
+        <h1>Evander's Notes App</h1>
         <input
             onChange={e => setFormData({ ...formData, 'name': e.target.value})}
             placeholder="Note name"
